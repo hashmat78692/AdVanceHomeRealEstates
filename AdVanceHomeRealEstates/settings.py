424b17c64296170da793d8b9b96116276567a58b
Hashmat Sulthana Shaikh <hshaikh@unomaha.edu>
2023-11-11 11:56:29 -0600

    Code update v1

--- AdVanceHomeRealEstates/settings.py
+++ AdVanceHomeRealEstates/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'advancehomerealestatesapp',
+    'crispy_forms',
+    'bootstrap4',
+    'crispy_bootstrap4',
+    'users',
     'django.contrib.staticfiles',
 ]
 
@@ -118,7 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
+LOGIN_REDIRECT_URL = "/"
