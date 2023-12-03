907461266de9dd74898b463e0b1e2a25ee5a9d07
Navya Daram <navyadaram@Navyas-MacBook-Pro.local>
2023-12-03 13:05:20 -0600

    image fun

--- listings/static/listings.js
+++ listings/static/listings.js
@@ -1,4 +1,56 @@
-function SaveListing() {
+$("div#my-awesome-dropzone").dropzone();
+$("div#my-awesome-dropzone1").dropzone();
+$("div#my-awesome-dropzone2").dropzone();
+$("div#my-awesome-dropzone3").dropzone();
+
+Dropzone.options.myawesomedropzone = { // camelized version of the `id`
+    paramName: "file", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    maxFiles: 2,
+    uploadMultiple: true,
+    accept: function(file, done) {
+      if (file.name == "justinbieber.jpg") {
+        done("Naha, you don't.");
+      }
+      else { done(); }
+    }
+  };
+
+  Dropzone.options.myawesomedropzone1 = { // camelized version of the `id`
+    paramName: "file2", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    maxFiles: 1,
+    accept: function(file, done) {
+      if (file.name == "justinbieber.jpg") {
+        done("Naha, you don't.");
+      }
+      else { done(); }
+    }
+  };
+  Dropzone.options.myawesomedropzone2 = { // camelized version of the `id`
+    paramName: "file3", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    maxFiles: 1,
+    accept: function(file, done) {
+      if (file.name == "justinbieber.jpg") {
+        done("Naha, you don't.");
+      }
+      else { done(); }
+    }
+  };
+  Dropzone.options.myawesomedropzone3 = { // camelized version of the `id`
+    paramName: "file4", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    maxFiles: 1,
+    accept: function(file, done) {
+      if (file.name == "justinbieber.jpg") {
+        done("Naha, you don't.");
+      }
+      else { done(); }
+    }
+  };
+
+function SaveListing(id) {
 
 var listingData = new Object();
 listingData.listingDate = $('#listingDate').val();
@@ -15,39 +67,27 @@ listingData.neighbourhood = $('#neighbourhood').val();
 listingData.propertyType = $('#propertyType').val();
 listingData = JSON.stringify(listingData);
 
-const propertyImage1 = $('#propertyImage1').get(0).files[0];;
+const propertyImage1 = $('#propertyImage1').get(0).files[0];
+const propertyImage2 = $('#propertyImage2').get(0).files[0];
+const propertyImage3 = $('#propertyImage3').get(0).files[0];
+const propertyImage4 = $('#propertyImage4').get(0).files[0];
 console.log(propertyImage1)
-//const propertyImage2 = $('#propertyImage2').prop('files')[0];
-//const propertyImage3 = $('#propertyImage3').prop('files')[0];
-//const propertyImage4 = $('#propertyImage4').prop('files')[0];
+var Data = new FormData();
+Data.append("listingData", listingData)
+Data.append("image1", propertyImage1)
+Data.append("image2", propertyImage2)
+Data.append("image3", propertyImage3)
+Data.append("image4", propertyImage4)
+Data.append("csrfmiddlewareoken", '{{ csrf_token }}')
 
-var Image1 = new FormData();
-Image1.append("image1", propertyImage1)
-Image1.append("csrfmiddlewareoken", '{{ csrf_token }}')
-
-//
-//var Image2 = new FormData();
-//Image2.append("image2", propertyImage2)
-//
-//var Image3 = new FormData();
-//Image3.append("image3", propertyImage3)
-//
-//var Image4 = new FormData();
-//Image4.append('image4', propertyImage4)
-
-
-console.log(Image1);
-//console.log(Image2);
-//console.log(Image3);
-//console.log(Image4);
 
   $.ajax({
-  url: "/save-listings",
+  url: "/save-listings/"+id,
   type: "POST",
   processData: false,
   contentType: false,
   mimeType:"multipart/form-data",
-  data: {'listingData': listingData,'listingImage1':"Image1"},
+  data:Data,
   success: (data) => {
     console.log(data);
     if(data == "success")
--- listings/views.py
+++ listings/views.py
@@ -20,28 +20,14 @@ def listings(request):
 
 
 @csrf_exempt
-def savelistings(request):
+def savelistings(request,id):
     if request.method == 'POST':
-        print(request.__dict__, file=sys.stderr)
-        listingData = request.POST.get("listingData")
-        print(listingData)
-        #listingImage1 = request.POST.get("listingImage1")
-        #fss1 = FileSystemStorage()
-        #print(listingImage1)
-        #filename1 = fss1.save(listingImage1.name,listingImage1)
-        #url1 = fss1.url(filename1)
-        # listingImage2 = request.FILES.get("listingImage2")
-        # fss2 = FileSystemStorage()
-        # filename2 = fss2.save(listingImage2.name, listingImage2)
-        # url2 = fss2.url(filename2)
-        # listingImage3 = request.FILES.get("listingImage3")
-        # fss3 = FileSystemStorage()
-        # filename3 = fss3.save(listingImage3.name, listingImage3)
-        # url3 = fss3.url(filename3)
-        # listingImage4 = request.FILES.get("listingImage4")
-        # fss4 = FileSystemStorage()
-        # filename4 = fss4.save(listingImage4.name, listingImage4)
-        # url4 = fss4.url(filename4)
+        print(id)
+        listingData = request.POST['listingData']
+        image1 = request.FILES['image1']
+        image2 = request.FILES['image2']
+        image3 = request.FILES['image3']
+        image4 = request.FILES['image4']
         listingData_json = json.loads(listingData)
         propertyListing = property_listing()
         propertyListing.admin_id = 1;
@@ -57,10 +43,10 @@ def savelistings(request):
         propertyListing.property_neighbourhood_id= listingData_json['neighbourhood']
         propertyListing.property_type_id = listingData_json['propertyType']
         propertyListing.property_listing_is_featured = to_boolean(listingData_json['featuredPropertyIndicator'])
-        # propertyListing.property_listing_pic1 = url1
-        # propertyListing.property_listing_pic2 = url2
-        # propertyListing.property_listing_pic3 = url3
-        # propertyListing.property_listing_pic4 = url4
+        propertyListing.property_listing_pic1 = image1
+        propertyListing.property_listing_pic2 = image2
+        propertyListing.property_listing_pic3 = image3
+        propertyListing.property_listing_pic4 = image4
         propertyListing.save()
         context = {
             'new_article_id': property_listing.pk,
@@ -73,16 +59,6 @@ def to_boolean(raw_value: str) -> bool:
         raw_value = str(raw_value)
     raw_value = raw_value.strip()
     return {'true': True, 'false': False}.get(raw_value.lower(), False)
-@csrf_exempt
-def saveImages(request):
-    if request.method == "POST":
-        f=request.FILES
-        print(f)
-        files = request.FILES.get('data', None)
-
-        print(files)
-        return JsonResponse({"Message": "File Uploaded Successfully"})
-    return JsonResponse({"Message": ""})
 
 @csrf_exempt
 def editlistings(request,id):
